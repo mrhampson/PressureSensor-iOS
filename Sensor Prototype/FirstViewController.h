e641959b2db5b877cb60ef2cfc0b9044442b211f
Bill Otwell <billotwell@Surbhis-MacBook-Pro.local>
2015-02-28 18:44:27 -0800

    Initial Commit

---
+++ Sensor Prototype/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Sensor Prototype
+//
+//  Created by Bill Otwell on 2/28/15.
+//  Copyright (c) 2015 BRIM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Sensor Prototype/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Sensor Prototype
+//
+//  Created by Bill Otwell on 2/28/15.
+//  Copyright (c) 2015 BRIM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
