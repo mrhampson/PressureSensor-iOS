326f1bc99cd78ea9855bbe008fceb3e5c3a30240
Marshall Hampson <hampson.mr@gmail.com>
2015-04-03 19:39:43 -0700

    Created new UI framework to build our app off of

---
+++ Mobile Medicine/Mobile Medicine/DataViewPortrait.swift
@@ -0,0 +1,57 @@
+//
+//  DataViewPortrait.swift
+//  Mobile Medicine
+//
+//  Created by Marshall Hampson on 4/3/15.
+//  Copyright (c) 2015 BRIM. All rights reserved.
+//
+
+import UIKit
+
+class DataViewPortrait: UIViewController {
+
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.Portrait.rawValue)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        UIDevice.currentDevice().beginGeneratingDeviceOrientationNotifications()
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "orientationChanged:", name: UIDeviceOrientationDidChangeNotification, object: nil)
+        
+    }
+    
+    func orientationChanged(notification: NSNotification){
+        let deviceOrientation = UIDevice.currentDevice().orientation;
+        if (UIDeviceOrientationIsLandscape(deviceOrientation)){
+            self.performSegueWithIdentifier("DataToLandscape", sender: self)
+            
+        }
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mobile Medicine/Mobile Medicine/IntroViewController.swift
@@ -0,0 +1,25 @@
+//
+//  IntroViewController.swift
+//  Mobile Medicine
+//
+//  Created by Marshall Hampson on 4/3/15.
+//  Copyright (c) 2015 BRIM. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
