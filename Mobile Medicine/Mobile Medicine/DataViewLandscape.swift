a73e098f96904acc1177576b8d33f3dd05137255
Marshall Hampson <hampson.mr@gmail.com>
2015-04-15 23:32:14 -0700

    Added the graph to DataViewLandscape.  Graph no has a header with the title as well as a colored line.

--- Mobile Medicine/Mobile Medicine/DataViewLandscape.swift
+++ Mobile Medicine/Mobile Medicine/DataViewLandscape.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class DataViewLandscape: UIViewController {
-
+class DataViewLandscape: UIViewController, JBLineChartViewDataSource, JBLineChartViewDelegate {
+    let _headerHeight:CGFloat = 80
+    let _footerHeight:CGFloat = 40
+    let _padding:CGFloat = 10
+    var graphData:[CGFloat] = []
+    let chartHeaderView = ChartHeaderView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+    
+    
     override func supportedInterfaceOrientations() -> Int {
         return Int(UIInterfaceOrientationMask.Landscape.rawValue)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        graphData = [1.0, 2.0, 3.0, 4.0];
+        
+        
+        
+        let lineChartView = JBLineChartView();
+        lineChartView.dataSource = self;
+        lineChartView.delegate = self;
+        lineChartView.backgroundColor = UIColor.darkGrayColor();
+        lineChartView.frame = CGRectMake(0,
+            0,
+            UIScreen.mainScreen().applicationFrame.height,
+            UIScreen.mainScreen().applicationFrame.width);
+        lineChartView.reloadData();
+        self.view.addSubview(lineChartView);
+        
+        chartHeaderView.frame =  CGRectMake(_padding,ceil(self.view.bounds.size.height * 0.5) - ceil(_headerHeight * 0.5),self.view.bounds.width - _padding*2, _headerHeight);
+        chartHeaderView.titleLabel.text = "Temperature vs Time";
+        lineChartView.headerView = chartHeaderView;
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -41,15 +67,38 @@ class DataViewLandscape: UIViewController {
         }
         
     }
-
+    
+    func numberOfLinesInLineChartView(lineChartView: JBLineChartView!) -> UInt {
+        return 1;
+    }
+    
+    func lineChartView(lineChartView: JBLineChartView!, numberOfVerticalValuesAtLineIndex lineIndex: UInt) -> UInt {
+        return UInt(graphData.count);
+    }
+    
+    func lineChartView(lineChartView: JBLineChartView!, verticalValueForHorizontalIndex horizontalIndex: UInt, atLineIndex lineIndex: UInt) -> CGFloat {
+        return graphData[Int(horizontalIndex)];
+    }
+    
+    func lineChartView(lineChartView: JBLineChartView!, colorForLineAtLineIndex lineIndex: UInt) -> UIColor! {
+        return uicolorFromHex(0x34b234)
+    }
+    
+    func uicolorFromHex(rgbValue:UInt32)->UIColor{
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+        
+        return UIColor(red:red, green:green, blue:blue, alpha:1.0)
+    }
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
