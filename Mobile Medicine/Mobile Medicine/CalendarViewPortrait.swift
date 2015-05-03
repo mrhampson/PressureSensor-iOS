40ced826f8f278333ea739f578bb2fc08eeabb6b
Conor Woods <conor@Conor-Woods.local>
2015-05-02 17:43:51 -0700

    Adding the calendar. There's some silly stuff with the CVCalendar files, there are two copies in there and one is superfluous, but anyways we can view the calendar now

--- Mobile Medicine/Mobile Medicine/CalendarViewPortrait.swift
+++ Mobile Medicine/Mobile Medicine/CalendarViewPortrait.swift
@@ -1,76 +1,195 @@
 //
-//  CalendarViewPortrait.swift
-//  Mobile Medicine
+//  ViewController.swift
+//  CVCalendar Demo
 //
-//  Created by Marshall Hampson on 4/3/15.
-//  Copyright (c) 2015 BRIM. All rights reserved.
+//  Created by Мак-ПК on 1/3/15.
+//  Copyright (c) 2015 GameApp. All rights reserved.
 //
 
 import UIKit
 
-class CalendarViewPortrait: UIViewController {
+class ViewController: UIViewController {
+    // MARK: - Properties
+    @IBOutlet weak var calendarView: CVCalendarView!
+    @IBOutlet weak var menuView: CVCalendarMenuView!
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var daysOutSwitch: UISwitch!
     
-    var isShowingLandscapeView = false
+    var shouldShowDaysOut = true
+    var animationFinished = true
     
-    override func shouldAutorotate() -> Bool {
+    // MARK: - Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        monthLabel.text = CVDate(date: NSDate()).globalDescription
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        calendarView.commitCalendarViewUpdate()
+        menuView.commitMenuViewUpdate()
+    }
+}
+
+// MARK: - CVCalendarViewDelegate
+
+extension ViewController: CVCalendarViewDelegate {
+    func presentationMode() -> CalendarMode {
+        return .MonthView
+    }
+    
+    func firstWeekday() -> Weekday {
+        return .Sunday
+    }
+    
+    func shouldShowWeekdaysOut() -> Bool {
+        return shouldShowDaysOut
+    }
+    
+    func didSelectDayView(dayView: CVCalendarDayView) {
+        let date = dayView.date
+        println("\(calendarView.presentedDate.commonDescription) is selected!")
+    }
+    
+    func presentedDateUpdated(date: CVDate) {
+        if monthLabel.text != date.globalDescription && self.animationFinished {
+            let updatedMonthLabel = UILabel()
+            updatedMonthLabel.textColor = monthLabel.textColor
+            updatedMonthLabel.font = monthLabel.font
+            updatedMonthLabel.textAlignment = .Center
+            updatedMonthLabel.text = date.globalDescription
+            updatedMonthLabel.sizeToFit()
+            updatedMonthLabel.alpha = 0
+            updatedMonthLabel.center = self.monthLabel.center
+            
+            let offset = CGFloat(48)
+            updatedMonthLabel.transform = CGAffineTransformMakeTranslation(0, offset)
+            updatedMonthLabel.transform = CGAffineTransformMakeScale(1, 0.1)
+            
+            UIView.animateWithDuration(0.35, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+                self.animationFinished = false
+                self.monthLabel.transform = CGAffineTransformMakeTranslation(0, -offset)
+                self.monthLabel.transform = CGAffineTransformMakeScale(1, 0.1)
+                self.monthLabel.alpha = 0
+                
+                updatedMonthLabel.alpha = 1
+                updatedMonthLabel.transform = CGAffineTransformIdentity
+                
+                }) { _ in
+                    
+                    self.animationFinished = true
+                    self.monthLabel.frame = updatedMonthLabel.frame
+                    self.monthLabel.text = updatedMonthLabel.text
+                    self.monthLabel.transform = CGAffineTransformIdentity
+                    self.monthLabel.alpha = 1
+                    updatedMonthLabel.removeFromSuperview()
+            }
+            
+            self.view.insertSubview(updatedMonthLabel, aboveSubview: self.monthLabel)
+        }
+    }
+    
+    func topMarker(shouldDisplayOnDayView dayView: CVCalendarDayView) -> Bool {
+        return true
+    }
+    
+    func dotMarker(shouldShowOnDayView dayView: CVCalendarDayView) -> Bool {
+        let day = dayView.date.day
+        let randomDay = Int(arc4random_uniform(31))
+        if day == randomDay {
+            return true
+        }
+        
         return false
     }
     
-    
-    override func supportedInterfaceOrientations() -> Int {
-        return Int(UIInterfaceOrientationMask.Portrait.rawValue)
-    }
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        UIDevice.currentDevice().beginGeneratingDeviceOrientationNotifications()
-        let notificationCenter = NSNotificationCenter.defaultCenter()
-        notificationCenter.addObserver(self, selector: "orientationChanged:", name: UIDeviceOrientationDidChangeNotification, object: nil)
+    func dotMarker(colorOnDayView dayView: CVCalendarDayView) -> UIColor {
+        let day = dayView.date.day
         
-    }
-    
-   
-    func orientationChanged(notification: NSNotification){
-        let deviceOrientation = UIDevice.currentDevice().orientation;
-        if (UIDeviceOrientationIsLandscape(deviceOrientation) && !isShowingLandscapeView){
-            self.performSegueWithIdentifier("CalToLandscape", sender: self)
-            isShowingLandscapeView = true
-            
-        }
-        else if(UIDeviceOrientationIsPortrait(deviceOrientation) && isShowingLandscapeView){
-            self.dismissViewControllerAnimated(true, completion: nil)
-            isShowingLandscapeView = false
-        }
+        let red = CGFloat(arc4random_uniform(600) / 255)
+        let green = CGFloat(arc4random_uniform(600) / 255)
+        let blue = CGFloat(arc4random_uniform(600) / 255)
         
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
+        return color
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if(segue.identifier == "CalToData"){
-            let notificationCenter = NSNotificationCenter.defaultCenter()
-            notificationCenter.removeObserver(self, name: UIDeviceOrientationDidChangeNotification, object: nil)
+    func dotMarker(shouldMoveOnHighlightingOnDayView dayView: CVCalendarDayView) -> Bool {
+        return true
+    }
+}
+
+// MARK: - CVCalendarViewAppearanceDelegate
+
+extension ViewController: CVCalendarViewAppearanceDelegate {
+    func dayLabelPresentWeekdayInitallyBold() -> Bool {
+        return false
+    }
+    
+    func spaceBetweenDayViews() -> CGFloat {
+        return 2
+    }
+}
+
+// MARK: - CVCalendarMenuViewDelegate
+
+extension ViewController: CVCalendarMenuViewDelegate {
+    // firstWeekday() has been already implemented.
+}
+
+// MARK: - IB Actions
+
+extension ViewController {
+    @IBAction func switchChanged(sender: UISwitch) {
+        if sender.on {
+            calendarView.changeDaysOutShowingState(false)
+            shouldShowDaysOut = true
+        } else {
+            calendarView.changeDaysOutShowingState(true)
+            shouldShowDaysOut = false
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func todayMonthView() {
+        calendarView.toggleCurrentDayView()
     }
-    */
+    
+    /// Switch to WeekView mode.
+    @IBAction func toWeekView(sender: AnyObject) {
+        calendarView.changeMode(.WeekView)
+    }
+    
+    /// Switch to MonthView mode.
+    @IBAction func toMonthView(sender: AnyObject) {
+        calendarView.changeMode(.MonthView)
+    }
+    
+    @IBAction func loadPrevious(sender: AnyObject) {
+        calendarView.loadPreviousView()
+    }
+    
+    
+    @IBAction func loadNext(sender: AnyObject) {
+        calendarView.loadNextView()
+    }
+}
 
+// MARK: - Convenience API Demo
+
+extension ViewController {
+    func toggleMonthViewWithMonthOffset(offset: Int) {
+        let calendar = NSCalendar.currentCalendar()
+        let calendarManager = calendarView.manager
+        let components = Manager.componentsForDate(NSDate()) // from today
+        
+        components.month += offset
+        
+        let resultDate = calendar.dateFromComponents(components)!
+        
+        self.calendarView.toggleViewWithDate(resultDate)
+    }
 }
