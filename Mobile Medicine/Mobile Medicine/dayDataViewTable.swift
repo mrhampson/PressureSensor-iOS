6db5c82ef641c0c519e88376be1f2bb6dbfa5c70
Conor Woods <conor@campus-061-078.ucdavis.edu>
2015-05-04 11:44:35 -0700

    Can now view each data run on a given day, some problems with timezones still need to be fixed

--- Mobile Medicine/Mobile Medicine/dayDataViewTable.swift
+++ Mobile Medicine/Mobile Medicine/dayDataViewTable.swift
@@ -18,6 +18,7 @@ class dayDataViewTable: UITableViewController {
     var insertDataInfo:NSManagedObject?
     var insertData:NSMutableOrderedSet = []
     var appDel:AppDelegate!
+    var tableElements: [NSManagedObject]! = []
     
     convenience init()
     {
@@ -30,32 +31,87 @@ class dayDataViewTable: UITableViewController {
         dataEntity = NSEntityDescription.entityForName("RecordData", inManagedObjectContext: context)!
         //insertDataInfo = NSManagedObject(entity: infoEntity, insertIntoManagedObjectContext: context)
     }
+    required init(coder aDecoder: NSCoder)
+    {
+        
+        //Core data context
+        appDel = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        context = appDel.managedObjectContext
+        infoEntity = NSEntityDescription.entityForName("RecordInfo", inManagedObjectContext: context)!
+        dataEntity = NSEntityDescription.entityForName("RecordData", inManagedObjectContext: context)!
+        //insertDataInfo = NSManagedObject(entity: infoEntity, insertIntoManagedObjectContext: context)
+        //insertDataInfo = NSEntityDescription.insertNewObjectForEntityForName ("RecordInfo", inManagedObjectContext: context) as! RecordInfo
+        super.init(coder: aDecoder)
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.registerClass(UITableViewCell.self,
+            forCellReuseIdentifier: "Cell")
         var error: NSError?
-        
         let fetchRequest = NSFetchRequest(entityName:"RecordInfo")
-        if error != nil
-        {
-            let fetchedResults = context.executeFetchRequest(fetchRequest, error: &error) as? [NSManagedObject]
-            println("Is this executed")
+
             
-            if let results = fetchedResults {
-                for result in results{
-                    println(result.valueForKey("rName"))
-                    println(result.valueForKey("rDate"))
-                    let dataArray = (result.valueForKey("dataRelation")) as! NSOrderedSet
-                    for data in dataArray{
-                        print(data.valueForKey("rData"), " ")
+        if let fetchedResults = context.executeFetchRequest(fetchRequest, error: &error)
+        {
+            let fetchedResultsOne = fetchedResults as? [NSManagedObject]
+            if let results = fetchedResultsOne
+            {
+                for result in results
+                {
+                    let rDay = result.valueForKey("rDate") as? NSDate
+                    let cDay = date.convertedDate()
+                    let next = cDay!.dateByAddingTimeInterval( (24 * 60 * 60 - 8*60*60) )
+                    let prev = cDay!.dateByAddingTimeInterval( (-24 * 60 * 60 - 8*60*60))
+                    if rDay?.earlierDate(next) == rDay && rDay?.laterDate(prev) == rDay
+                    {
+                        println(result.valueForKey("rName"))
+                        println(result.valueForKey("rDate"))
+                        let dataArray = (result.valueForKey("dataRelation")) as! NSOrderedSet
+                        if !result.isEqual(nil)
+                        {
+                            tableElements.append(result)
+                            println("Appended value")
+                        }
+                        for data in dataArray
+                        {
+                            print(data.valueForKey("rData"), " ")
+                        }
                     }
                     println()
                 }
             }
-            
         }
 
+        tableView.reloadData()
         // Do view setup here.
+    }
+    
+    override func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            if tableElements == nil
+            {
+                return 0
+            }
+            else
+            {
+                return tableElements!.count
+            }
+    }
+    
+    override func tableView(tableView: UITableView,
+        cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+            
+            let cell =
+            tableView.dequeueReusableCellWithIdentifier("Cell")
+                as! UITableViewCell
+            
+            let row = tableElements[indexPath.row]
+            cell.textLabel!.text = row.valueForKey("rName") as? String
+            
+            return cell
     }
     
 }
