f1bbc8fc21b6c3c22425309fb9b02d343ef812bd
Conor Woods <conor@campus-001-142.ucdavis.edu>
2015-05-19 13:28:29 -0700

    Starting to get the list view working

---
+++ Mobile Medicine/Mobile Medicine/ListDataHeader.swift
@@ -0,0 +1,16 @@
+//
+//  ListDataHeader.swift
+//  Mobile Medicine
+//
+//  Created by Conor Woods on 5/18/15.
+//  Copyright (c) 2015 BRIM. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ListDataHeader: UITableViewCell {
+
+    @IBOutlet weak var HeaderLabel: UILabel!
+
+}
--- Mobile Medicine/Mobile Medicine/ListDataView.swift
+++ Mobile Medicine/Mobile Medicine/ListDataView.swift
@@ -1,16 +1,74 @@
 //
-//  dayDataViewTable.swift
+//  ListDataView.swift
 //  Mobile Medicine
 //
-//  Created by Conor Woods on 5/3/15.
+//  Created by Conor Woods on 5/18/15.
 //  Copyright (c) 2015 BRIM. All rights reserved.
 //
 
 import UIKit
 import CoreData
 
-class dayDataViewTable: UITableViewController {
+class ListDataView: UITableViewController {
+/*
+    
+    var countriesinEurope = ["France","Spain","Germany"]
+    var countriesinAsia = ["Japan","China","India"]
+    var countriesInSouthAmerica = ["Argentia","Brasil","Chile"]
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // 1
+        // Return the number of sections.
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 2
+        return 3
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        // 3
+        // Configure the cell...
+        switch (indexPath.section) {
+        case 0:
+            cell.textLabel?.text = countriesinEurope[indexPath.row]
+        case 1:
+            cell.textLabel?.text = countriesinAsia[indexPath.row]
+        case 2:
+            cell.textLabel?.text = countriesInSouthAmerica[indexPath.row]
+            //return sectionHeaderView
+        default:
+            cell.textLabel?.text = "Other"
+        }
+        
+        return cell
+    }
 
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let  headerCell = tableView.dequeueReusableCellWithIdentifier("HeaderCell") as! ListDataHeader
+        headerCell.backgroundColor = UIColor.lightGrayColor()
+        
+        switch (section) {
+        case 0:
+            headerCell.HeaderLabel.text = "Europe";
+            //return sectionHeaderView
+        case 1:
+            headerCell.HeaderLabel.text = "Asia";
+            //return sectionHeaderView
+        case 2:
+            headerCell.HeaderLabel.text = "South America";
+            //return sectionHeaderView
+        default:
+            headerCell.HeaderLabel.text = "Other";
+        }
+        
+        return headerCell
+    }*/
     internal var date:CVDate!
     var context:NSManagedObjectContext!
     var infoEntity:NSEntityDescription?
@@ -21,6 +79,9 @@ class dayDataViewTable: UITableViewController {
     var tableElements: [NSManagedObject]! = []
     //var dataToPass: [Double]! = []
     var selectedIndex: NSIndexPath!
+    var daysWithData: [String] = []
+    let calendar = NSCalendar.currentCalendar()
+    //var dataOnDay: [NSManagedObect]! = []
     
     convenience init()
     {
@@ -47,50 +108,63 @@ class dayDataViewTable: UITableViewController {
         
     }
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         tableView.registerClass(UITableViewCell.self,
             forCellReuseIdentifier: "Cell")
         var error: NSError?
+        //var dateHeader: NSDateComponents?
+        var inArray: Bool =  false
         let fetchRequest = NSFetchRequest(entityName:"RecordInfo")
-
-            
         if let fetchedResults = context.executeFetchRequest(fetchRequest, error: &error)
         {
+            let formatter = NSDateFormatter()
+            formatter.dateFormat = "MMM d, y"
             let fetchedResultsOne = fetchedResults as? [NSManagedObject]
             if let results = fetchedResultsOne
             {
                 for result in results
                 {
                     let rDay = result.valueForKey("rDate") as? NSDate
-                    let cDay = date.convertedDate()
-                    let next = cDay!.dateByAddingTimeInterval((24 * 60 * 60 - 8*60*60))
-                    //let prev = cDay!.dateByAddingTimeInterval( (-24 * 60 * 60 - 8*60*60))
-                    if rDay?.earlierDate(next) == rDay && rDay?.laterDate(cDay!) == rDay
+                    let dateString = formatter.stringFromDate(rDay!)
+                    //println(dateString)
+
+                    if daysWithData.count == 0
                     {
-                        //println(result.valueForKey("rName"))
-                        //println(result.valueForKey("rDate"))
-                        let dataArray = (result.valueForKey("dataRelation")) as! NSOrderedSet
-                        if !result.isEqual(nil)
+                            daysWithData.append(dateString)
+                    } // if daysWithData is empty, put in the first element
+                    else if (daysWithData.count > 0)
+                    {
+                        inArray = false
+                        for element in daysWithData
                         {
-                            tableElements.append(result)
-                            //println("Appended value")
+                            if element == dateString
+                            {
+                                inArray = true
+                            }
                         }
-                        /*for data in dataArray
+                        if inArray == false
                         {
-                            print(data.valueForKey("rData"), " ")
-                        }*/
+                            daysWithData.append(dateString)
+                        }
+                    } //if it's not empty, check to see if the day is in there, if not, add it
+                    let dataArray = (result.valueForKey("dataRelation")) as! NSOrderedSet
+                    if !result.isEqual(nil)
+                    {
+                        tableElements.append(result)
                     }
-                    println()
                 }
             }
         }
-
+        println(daysWithData)
         tableView.reloadData()
         // Do view setup here.
     }
     
-    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return daysWithData.count
+    }
     
     override func tableView(tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int {
@@ -117,7 +191,7 @@ class dayDataViewTable: UITableViewController {
             
             return cell
     }
-
+    
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         selectedIndex = indexPath
         self.performSegueWithIdentifier("dayDataViewToGraph", sender: self)
@@ -129,7 +203,7 @@ class dayDataViewTable: UITableViewController {
             graphView.dataName = tableElements[selectedIndex.row].valueForKey("rName") as! String
             let data = (tableElements[selectedIndex.row].valueForKey("dataRelation")) as! NSOrderedSet
             graphView.graphData = arrayHelper(data)
-
+            
         }
     }
     func arrayHelper(orderedSet: NSOrderedSet) -> [Double]
@@ -142,4 +216,16 @@ class dayDataViewTable: UITableViewController {
         }
         return(output)
     }
+    
+    /*func getNumDaysWithData(tableElements: [NSManagedObject]) -> Int
+    {
+    
+        for element in tableElements
+        {
+            if(element.)
+        }
+        println(numDays)
+        return numDays
+    }*/
 }
+
